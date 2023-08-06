492dc69c278cf52e519b255b35170f2396b7792a
sakitama14 <104491362+sakitama14@users.noreply.github.com>
2023-08-07 08:27:08 +0900

    react-todo 基盤づくり

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles.css";
 
-export default function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div>
+        <input textarea="値を入力" />
+        <button></button>
+      </div>
+      <div></div>
+      <div></div>
+    </>
   );
-}
+};
