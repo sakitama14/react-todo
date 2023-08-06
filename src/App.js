f937522cf8dd00a80d3bd34dad91a1d4b55a6c89
sakitama14 <104491362+sakitama14@users.noreply.github.com>
2023-08-07 08:40:00 +0900

    側の作成

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
-        <input textarea="値を入力" />
-        <button></button>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
       </div>
-      <div></div>
-      <div></div>
+      <div className="incomplete-area">
+        <p className="title">未完了のリスト</p>
+
+        <ul className="list-row">
+          <li>あああ</li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のリスト</p>
+        <ul className="list-row">
+          <li>あああ</li>
+          <button>戻す</button>
+        </ul>
+      </div>
     </>
   );
 };
